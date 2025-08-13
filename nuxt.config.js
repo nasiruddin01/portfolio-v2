bf5bc101337cf06ae247cbe26f04a58bb6f8332e
nasiruddin01 <nasiruddin.dev@gmail.com>
2025-08-13 22:12:55 +0600

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,21 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=VT323&display=swap",
         },
+        // Add these favicon-related links
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon.png" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon.png",
+        },
       ],
     },
   },
